b2d742284e003db448b9c91a4ef2e358f396a2c8
Stefano Harding <riddopic@gmail.com>
2015-04-03 20:32:56 -0700

    Update metadata and Berksfile

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'riddopic@gmail.com'
 license          'Apache 2.0'
 description      'Collection of utility helpers and methods.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           Garcon::VERSION.to_s
+version           Garcon::VERSION.string
 
 supports 'centos',      '>= 5.10'
 supports 'oracle',      '>= 5.10'
